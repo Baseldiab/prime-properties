395ac5d9016f85d1b93c587be7110e7732c09514
Basel Diab <114107867+Baseldiab@users.noreply.github.com>
2023-11-26 12:30:11 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        gradient: "rgba(0, 0, 0, 0.5)",
+      },
+    },
+  },
+  plugins: [],
+};
